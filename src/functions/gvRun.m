fad919dc5c2f48e04399c653119b70d11f95eeac
Erik Roberts <eriktheblue@gmail.com>
2018-04-30 13:32:54 -0400

    gvr alias

--- src/functions/gvRun.m
+++ src/functions/gvRun.m
@@ -1,3 +1,5 @@
 function gvObj = gvRun(varargin)
+  %gvRun - alias for gv.Run
+  
   gvObj = gv.Run(varargin{:});
 end
---
+++ src/functions/gvr.m
@@ -0,0 +1,5 @@
+function gvObj = gvr(varargin)
+  %gvr - alias for gv.Run
+  
+  gvObj = gv.Run(varargin{:});
+end
