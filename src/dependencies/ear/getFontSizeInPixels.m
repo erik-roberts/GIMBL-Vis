6676b7d1e7a2b5988e9f1abfd578abeeec7dbf4a
Erik Roberts <eriktheblue@gmail.com>
2019-03-16 11:47:24 -0400

    add fontsizeinpx normalized support

--- src/dependencies/ear/getFontSizeInPixels.m
+++ src/dependencies/ear/getFontSizeInPixels.m
@@ -1,16 +1,24 @@
 function [textWidth, textHeight] = getFontSizeInPixels(fontSize, s)
-% getFontSizeInPixels - get font size in pixels based on fontSize input
-%
-% Author: Erik Roberts
+% getFontSizeInPixels - get font size in pixels based on fontSize input (points or normalized)
+% e.g., 13 points = 0.04 normalized => 6.4 px width
+
 s.FontSize = fontSize;
 if nargin < 2
-  s.FontUnits = 'points';
+  if s.FontSize <= 1
+    s.FontUnits = 'normalized';
+  else
+    s.FontUnits = 'points';
+  end
   s.FontAngle = get(0,'defaultuicontrolFontAngle');
   s.FontName = get(0,'defaultuicontrolFontName');
   s.FontWeight = get(0,'defaultuicontrolFontWeight');
 end
 
-hFig = figure('Visible','off');
+if ~isfield(s, 'Position')
+  s.Position = [0 0 560 420]; % use ml2017b default figure size
+end
+
+hFig = figure('Visible','off', 'Position',s.Position);
 hAx = axes(hFig);
 
 % Get text size in data units
